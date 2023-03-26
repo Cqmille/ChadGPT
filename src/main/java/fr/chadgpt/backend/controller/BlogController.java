1c73c4a3cd95cecc19f6fe534e2aee27d9a6c44b
Cqmille <lafleur.art@gmail.com>
2023-03-26 21:17:13 +0200

    0.17 pagination, bootstrap

--- src/main/java/fr/chadgpt/backend/controller/BlogController.java
+++ src/main/java/fr/chadgpt/backend/controller/BlogController.java
@@ -4,13 +4,18 @@ import fr.chadgpt.backend.model.entity.BlogPost;
 import fr.chadgpt.backend.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class BlogController {
@@ -18,12 +23,26 @@ public class BlogController {
     @Autowired
     private BlogService blogService;
 
-    @GetMapping("/")
-    public String blogMain(Model model, @RequestParam(name = "page", defaultValue = "1") int page) {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String listPosts(Model model,
+                            @RequestParam("page") Optional<Integer> page) {
+
+        int currentPage = page.orElse(1);
         int pageSize = 5;
-        Page<BlogPost> blogPostsPage = blogService.findPaginated(page, pageSize);
-        model.addAttribute("blogPostsPage", blogPostsPage);
+
+        Page<BlogPost> postPage = blogService.findPaginated(currentPage, pageSize);
+
+        model.addAttribute("postPage", postPage);
+
+        int totalPages = postPage.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
 
         return "blog/index";
     }
+
 }
